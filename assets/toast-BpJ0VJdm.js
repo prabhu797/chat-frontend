85fd171da84e87b61d0bf688e8c6137b6d2c1eaf
prabhu797 <prabhudesai007@gmail.com>
2025-04-10 11:55:53 +0530

    Updates

---
+++ assets/toast-BpJ0VJdm.js
@@ -0,0 +1,1 @@
+import{k as c}from"./index-CUTMCaQ5.js";const a=(s,r)=>{switch(s){case"success":c.success(r);break;case"error":c.error(r);break;case"warning":c(r,{icon:"⚠️"});break;default:c(r)}};export{a as s};
